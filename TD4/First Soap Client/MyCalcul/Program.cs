db0282341f9d7db2d82ae8270a9a408cec0695ca
Asa <asareel56@gmail.com>
2021-03-16 14:41:06 +0100

    Added First SOAP Web Service Client

---
+++ TD4/First Soap Client/MyCalcul/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCalcul.Calculator;
+
+namespace MyCalcul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorSoapClient calculatorSoapClient = new CalculatorSoapClient();
+            Console.WriteLine(calculatorSoapClient.Add(1, 8));
+            Console.ReadKey();
+        }
+    }
+}
